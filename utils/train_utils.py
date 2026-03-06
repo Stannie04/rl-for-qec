10b177ca1987099c8ae97906a48887be2392da2a
stan <stan.ruijters@outlook.com>
2026-03-06 10:11:35 +0100

    Add optuna hyperparameter search.

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import time
 from stable_baselines3 import DQN, PPO, SAC
 from stable_baselines3.common.callbacks import BaseCallback
+import optuna
 
 from environments import MultivariateBicycleCode
 
@@ -59,6 +60,32 @@ def initialize_agents(config, env):
     return defender, adversary
 
 
+
+def single_agent_training_loop(
+    model_config,
+    code_config,
+    model_checkpoint=None
+):
+
+    rewards = {"Defender": []}
+    for _ in range(model_config["n_repetitions"]):
+
+        env = MultivariateBicycleCode(**code_config)
+
+        defender, adversary = initialize_agents(model_config["params"], env)
+
+        callback = RewardTrackerCallback()
+        defender.learn(total_timesteps=model_config["pretrain_timesteps"], progress_bar=True, callback=callback)
+
+        rewards["Defender"].append(callback.episode_rewards)
+
+    # Save agent
+    if model_checkpoint is not None:
+        defender.save(model_checkpoint)
+
+    return rewards
+
+
 def adversarial_training_loop(
     model_config,
     code_config,
@@ -94,6 +121,8 @@ def adversarial_training_loop(
             action, _ = actor.predict(obs, deterministic=False)
             next_obs, reward, terminated, truncated, info = env.step(action, single_player=False)
             done = terminated or truncated
+
+            # Required for SB3 replay buffer which expects info to be a list of dicts
             info = [info]
 
             actor.replay_buffer.add(obs, next_obs, action, reward, done, info)
@@ -121,3 +150,39 @@ def adversarial_training_loop(
         rewards["Defender"].append(all_rewards_defender)
         rewards["episode_steps"].append(episode_steps)
     return rewards
+
+
+def sample_sac_params(trial: optuna.Trial) -> dict:
+    """
+    Define the search space for SAC hyperparameters.
+    """
+    return {
+        "batch_size": trial.suggest_categorical("batch_size", [64, 128, 256, 512]),
+        "learning_rate": trial.suggest_float("learning_rate", 1e-5, 1e-3, log=True),
+        "buffer_size": trial.suggest_categorical("buffer_size", [100000, 200000, 500000]),
+        "gamma": trial.suggest_float("gamma", 0.95, 0.999),
+        "tau": trial.suggest_float("tau", 0.005, 0.02),
+        "train_freq": trial.suggest_categorical("train_freq", [1, 4, 8]),
+        "gradient_steps": trial.suggest_categorical("gradient_steps", [1, 4, 8]),
+        "ent_coef": trial.suggest_float("ent_coef", 1e-4, 1e-1, log=True),
+    }
+
+
+def objective(trial: optuna.Trial, model_config, code_config) -> float:
+    """
+    Objective function for Optuna.
+    """
+    env = MultivariateBicycleCode(**code_config)
+
+    # Sample hyperparameters
+    hyperparams = sample_sac_params(trial)
+    model = SAC("MlpPolicy", env, verbose=0, **hyperparams)
+
+    # Train
+    callback = RewardTrackerCallback()
+    model.learn(total_timesteps=50_000, callback=callback)
+
+    # Evaluate mean reward over last 100 episodes
+    mean_reward = np.mean(callback.episode_rewards[-100:])
+
+    return mean_reward
