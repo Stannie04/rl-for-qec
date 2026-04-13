d403ef356b071230f7e94f1c49560db4acc26003
stan <stan.ruijters@outlook.com>
2026-04-13 11:39:32 +0200

    Fix DQN taking in references of Data objects, causing replay buffer entries to be modified during training.

--- agents/dqn.py
+++ agents/dqn.py
@@ -4,33 +4,42 @@ import copy
 import numpy as np
 import torch
 from .gnn import GNN
-from torch_geometric.data import Batch
+from torch_geometric.data import Batch, Data
 import torch.nn.functional as F
 
+torch.set_float32_matmul_precision('high')
+
 class ReplayBuffer:
-    def __init__(self, capacity=10000):
+    def __init__(self, capacity=10000, device=torch.device("cpu")):
         self.buffer = deque(maxlen=capacity)
+        self.device = device
 
     def push(self, state, action, reward, next_state, done):
-        self.buffer.append((
-            copy.deepcopy(state),
-            action,
-            reward,
-            copy.deepcopy(next_state),
-            done
-        ))
+
+        data = state.clone()
+        next_data = next_state.clone()
+
+        self.buffer.append((data, action, reward, next_data, done))
 
     def sample(self, batch_size):
+
         batch = random.sample(self.buffer, batch_size)
         states, actions, rewards, next_states, dones = zip(*batch)
-        return list(states), list(actions), list(rewards), list(next_states), list(dones)
+
+        state_batch = Batch.from_data_list(states)
+        actions = torch.tensor(actions, dtype=torch.long, device=self.device)
+        rewards = torch.tensor(rewards, dtype=torch.float32, device=self.device)
+        dones = torch.tensor(dones, dtype=torch.float32, device=self.device)
+        next_state_batch = Batch.from_data_list(next_states)
+
+        return state_batch, actions, rewards, next_state_batch, dones
 
     def __len__(self):
         return len(self.buffer)
 
+
 class DQNAgent:
     def __init__(self, env, **kwargs):
-        self.replay_buffer = ReplayBuffer(capacity=kwargs.get("replay_buffer_capacity", 10000))
         self.batch_size = kwargs.get("batch_size", 64)
         self.gamma = kwargs.get("gamma", 0.99)
         self.epsilon_start = kwargs.get("epsilon_start", 1.0)
@@ -41,20 +50,31 @@ class DQNAgent:
         self.num_timesteps = kwargs.get("num_timesteps", 100000)
 
         self.env = env
+        self.replay_buffer = ReplayBuffer(capacity=kwargs.get("replay_buffer_capacity", 10000), device=self.device)
+
         self.model = GNN().to(self.device)
         self.target_model = copy.deepcopy(self.model)
+
+        self.model = torch.compile(self.model, mode="reduce-overhead")
+        self.target_model = torch.compile(self.target_model, mode="reduce-overhead")
+        # self.train_step = torch.compile(self.train_step)
+
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=kwargs.get("learning_rate", 1e-3))
+        self.eval_freq = kwargs.get("eval_freq", 10000)
+
+        self.evaluation_mode = kwargs.get("evaluation_mode", False)
 
         self.steps = 0
+        self.num_nodes = self.env.data.num_nodes
 
 
     def select_action(self, state):
         epsilon = self.epsilon_end + (self.epsilon_start - self.epsilon_end) * np.exp(-1. * self.steps / self.epsilon_decay)
-        if random.random() < epsilon:
+        if random.random() < epsilon and not self.evaluation_mode:
             return self.env.action_space.sample()
 
         with torch.no_grad():
-            q_values = self.model(state)[:self.env.action_space.n]
+            q_values = self.model(state.x, state.edge_index)[:self.env.action_space.n]
             return q_values.argmax().item()
 
 
@@ -62,30 +82,24 @@ class DQNAgent:
         if len(self.replay_buffer) < self.batch_size:
             return None
 
-        states, actions, rewards, next_states, dones = self.replay_buffer.sample(self.batch_size)
+        state_batch, actions, rewards, next_state_batch, dones = self.replay_buffer.sample(self.batch_size)
 
-        state_batch = Batch.from_data_list(states).to(self.device)
-        next_state_batch = Batch.from_data_list(next_states).to(self.device)
+        q_all = self.model(state_batch.x, state_batch.edge_index)
+        q_all = q_all.view(self.batch_size, self.num_nodes, -1)
+        q_all = q_all[:, :self.env.n_data, 0]
 
-        actions = torch.tensor(actions, device=self.device)
-        rewards = torch.tensor(rewards, dtype=torch.float32, device=self.device)
-        dones = torch.tensor(dones, dtype=torch.float32, device=self.device)
-
-        q_values = self.model(state_batch)  # [total_nodes]
-
-        # Extract per-graph qubit Q-values
-        q_values = q_values.view(self.batch_size, -1)[:, :self.env.n_data]
-        q = q_values.gather(1, actions.unsqueeze(1)).squeeze(1)
-
+        q = q_all.gather(1, actions.unsqueeze(1)).squeeze(1)
         with torch.no_grad():
 
-            next_q_online = self.model(next_state_batch)
-            next_q_online = next_q_online.view(self.batch_size, -1)[:, :self.env.n_data]
+            next_q_online = self.model(next_state_batch.x, next_state_batch.edge_index)
+            next_q_online = next_q_online.reshape(self.batch_size, self.num_nodes)
+            next_q_online = next_q_online[:, :self.env.n_data]
 
-            next_actions = torch.argmax(next_q_online, dim=1)
+            next_actions = next_q_online.argmax(dim=1)
 
-            next_q_target = self.target_model(next_state_batch)
-            next_q_target = next_q_target.view(self.batch_size, -1)[:, :self.env.n_data]
+            next_q_target = self.target_model(next_state_batch.x, next_state_batch.edge_index)
+            next_q_target = next_q_target.reshape(self.batch_size, self.num_nodes)
+            next_q_target = next_q_target[:, :self.env.n_data]
 
             next_q = next_q_target.gather(1, next_actions.unsqueeze(1)).squeeze(1)
 
